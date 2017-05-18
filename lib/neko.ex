82f64fd46e8808e49ba3fce00269f6749950277c
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2017-05-18 18:07:07 +0300

    initial commit

---
+++ lib/neko.ex
@@ -0,0 +1,18 @@
+defmodule Neko do
+  @moduledoc """
+  Documentation for Neko.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Neko.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/neko_test.exs
@@ -0,0 +1,8 @@
+defmodule NekoTest do
+  use ExUnit.Case
+  doctest Neko
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
