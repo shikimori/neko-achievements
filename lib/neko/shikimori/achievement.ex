e5af2db4c0d695eff33b9033d9aededfe7c6ed9f
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2017-06-13 12:34:46 +0300

    misc

--- lib/neko/shikimori/achievement.ex
+++ lib/neko/shikimori/achievement.ex
@@ -1,7 +1,7 @@
 defmodule Neko.Shikimori.Achievement do
   import Neko.Shikimori, only: [make_request!: 3]
 
-  def get_by_user(user_id) do
+  def get_by_user!(user_id) do
     make_request!(:get, "achievements", %{user_id: user_id})
     |> Poison.decode!(as: [%Neko.Achievement{}])
   end
