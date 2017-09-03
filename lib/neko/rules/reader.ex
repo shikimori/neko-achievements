fe164d0b6adb06c1f0378441fb532935e95febc4
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2017-09-03 21:58:49 +0300

    misc

--- lib/neko/rules/reader.ex
+++ lib/neko/rules/reader.ex
@@ -1,8 +1,8 @@
 defmodule Neko.Rules.Reader do
-  @rules_path Application.get_env(:neko, :rules)[:yml]
+  @rules_yml Application.get_env(:neko, :rules)[:yml]
 
   def read_from_file(type) do
-    Application.app_dir(:neko, @rules_path)
+    Application.app_dir(:neko, @rules_yml)
     |> YamlElixir.read_from_file()
     |> Enum.filter(&(&1["type"] == type))
   end
