efe004a6105e5d6279160a889fb62f78524b38e0
Alexey Terekhov <alexey.terekhov.ingate@gmail.com>
2017-10-05 00:49:44 +0300

    parse percent thresholds

--- lib/neko/rules/simple_rule/store.ex
+++ lib/neko/rules/simple_rule/store.ex
@@ -12,24 +12,9 @@ defmodule Neko.Rules.SimpleRule.Store do
   def load do
     Neko.Rules.Reader.read_from_files(@algo)
     |> Enum.map(&(Neko.Rules.SimpleRule.new(&1)))
-    |> calc_next_thresholds()
     |> calc_anime_ids()
-  end
-
-  defp calc_next_thresholds(rules) do
-    rules
-    |> Enum.map(fn(x) ->
-      %{x | next_threshold: next_threshold(rules, x)}
-    end)
-  end
-
-  defp next_threshold(rules, rule) do
-    rules
-    |> Enum.filter(fn(x) ->
-      x.neko_id == rule.neko_id and x.level == rule.level + 1
-    end)
-    |> Enum.map(&(&1.threshold))
-    |> List.first()
+    |> calc_thresholds()
+    |> calc_next_thresholds()
   end
 
   defp calc_anime_ids(rules) do
@@ -57,5 +42,37 @@ defmodule Neko.Rules.SimpleRule.Store do
 
   defp lists_overlap?(list_1, list_2) do
     !Enum.empty?(list_1 -- (list_1 -- list_2))
+  end
+
+  defp calc_thresholds(rules) do
+    rules
+    |> Enum.map(fn
+      %{threshold: threshold} = rule when is_integer(threshold) ->
+        rule
+      %{threshold: threshold} = rule when is_binary(threshold) ->
+        %{rule | threshold: parse_threshold(rule)}
+    end)
+  end
+
+  # when threshold is a string value, percent is implied
+  defp parse_threshold(rule) do
+    percent = rule.threshold |> Integer.parse() |> elem(0)
+    MapSet.size(rule.anime_ids) * percent / 100
+  end
+
+  defp calc_next_thresholds(rules) do
+    rules
+    |> Enum.map(fn(x) ->
+      %{x | next_threshold: next_threshold(rules, x)}
+    end)
+  end
+
+  defp next_threshold(rules, rule) do
+    rules
+    |> Enum.filter(fn(x) ->
+      x.neko_id == rule.neko_id and x.level == rule.level + 1
+    end)
+    |> Enum.map(&(&1.threshold))
+    |> List.first()
   end
 end
