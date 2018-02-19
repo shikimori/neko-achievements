005e8f568e6a2e935323ef5be32ca0e0145b824f
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2018-02-20 01:13:46 +0300

    update .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,7 @@
 [
+  # uncomment when plug package adopts .formatter.exs
+  #import_deps: [:plug],
   inputs: ["{config,lib,test}/**/*.{ex,exs}", "mix.exs"],
+  line_length: 83,
   locals_without_parens: [plug: 1, plug: 2]
 ]
