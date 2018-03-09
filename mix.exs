2be80b71568b53dc526071b1d256261a566d215e
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2018-03-09 04:40:36 +0300

    bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Neko.Mixfile do
   def project do
     [
       app: :neko,
-      version: "0.1.0",
-      elixir: "~> 1.4",
+      version: "0.2.0",
+      elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
