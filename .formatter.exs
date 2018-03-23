af114eaead0e97e917bc4b57bdc44d484b47f1c7
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2018-03-23 12:37:49 +0300

    use common formatter.exs (hardlink)

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,26 @@
 [
-  # uncomment when plug package adopts .formatter.exs
-  #import_deps: [:plug],
+  # uncomment these packages adopt .formatter.exs
+  #import_deps: [:ecto, :phoenix, :plug],
   inputs: ["{config,lib,test}/**/*.{ex,exs}", "mix.exs"],
   line_length: 83,
-  locals_without_parens: [plug: 1, plug: 2, raise: 1]
+  locals_without_parens: [
+    action_fallback: 1,
+    belongs_to: 2,
+    defenum: 2,
+    embeds_one: 2,
+    embeds_one: 3,
+    field: 2,
+    field: 3,
+    get: 3,
+    has_many: 2,
+    pipe_through: 1,
+    plug: 1,
+    plug: 2,
+    post: 3,
+    raise: 1,
+    resources: 3,
+    socket: 2,
+    timestamps: 1,
+    transport: 2
+  ]
 ]
