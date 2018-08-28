d0049de4e7cb746c905d80071e130a1047dbed7a
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2018-08-28 21:55:51 +0300

    misc

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,5 @@ use Mix.Config
 config :neko, :cowboy, listen_address: {192, 168, 0, 2}
 config :neko, :cowboy, listen_port: 4000
 
-# Do not include time - it's provided by systemd journal
+# Do not include time - it's printed by systemd journal
 config :logger, :console, format: "$metadata[$level] $message\n"
