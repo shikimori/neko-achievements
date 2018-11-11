1f17530a07d4e6bf6f6ef9e1ef17e146b2cd643f
Andrey Sidorov <takandar@gmail.com>
2018-11-11 13:01:49 +0300

    bugfix for local neko runs

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :neko, :cowboy, listen_address: {127, 0, 0, 1}
 config :neko, :cowboy, listen_port: 4004
 
-if System.get_env("SHIKIMORI_LOCAL") do
+if System.get_env("SHIKIMORI_LOCAL") == "true" do
   config :neko, :shikimori, url: "https://shikimori.local/api/"
 else
   config :neko, :shikimori, url: "https://shikimori.org/api/"
