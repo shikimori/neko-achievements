5e9249e7d0358926968835d1bece905b42fbb04d
Andrey Sidorov <takandar@gmail.com>
2019-09-26 11:19:18 +0300

    update rules

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ config :neko, :cowboy, listen_address: {127, 0, 0, 1}
 config :neko, :cowboy, listen_port: 4004
 
 if System.get_env("SHIKIMORI_LOCAL") == "true" do
-  config :neko, :shikimori, url: "https://shikimori.local/api/"
+  config :neko, :shikimori, url: "http://shikimori.local/api/"
 else
   config :neko, :shikimori, url: "https://shikimori.one/api/"
 end
