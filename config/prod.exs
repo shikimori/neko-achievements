5adc5c4fffb41000d0233bf63f6f4442c2100c87
Andrey Sidorov <takandar@gmail.com>
2021-04-12 18:56:53 +0300

    change production listen address

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :neko, :cowboy, listen_address: {192, 168, 0, 2}
+config :neko, :cowboy, listen_address: {127, 0, 0, 1}
 config :neko, :cowboy, listen_port: 4000
 
 # Do not include time - it's printed by systemd journal
